c70deb409d592456881ca222be6d0e7cfc6ae3b5
commenthropha <aharoon1007@gmail.com>
2024-06-13 20:40:40 +0100

    Create CPU struct in cpu.h

---
+++ cpu.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+struct CPU {
+    uint8_t *memory;
+    uint8_t *registers;
+    uint8_t *stack;
+    int I; // Index Register
+    int DT; // Delay Timer
+    int ST; // Sound Timer
+    int SP; // Stack Pointer
+};
