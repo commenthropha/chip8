26f869ddad01e9e74e1b9d3a9cc1a2494c531afa
commenthropha <aharoon1007@gmail.com>
2024-06-18 19:18:37 +0100

    Format screen.c error messages

--- src/screen.c
+++ src/screen.c
@@ -6,15 +6,15 @@
 void intitialise_screen(SDL_Renderer *renderer, SDL_Window *window, SDL_Texture *texture) {
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        fprintf(stderr, "\e[0;31mFailed to initialise the SDL2 library\e[0m", SDL_GetError());
-        return -1;
+        fprintf(stderr, "\e[0;31mFailed to initialise the SDL2 library\e[0m: %s", SDL_GetError());
+        exit(1);
     }
 
     window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 	
     if(!window)
     {
-        fprintf(stderr, "\e[0;31mFailed to create window\e[0m", SDL_GetError());
+        fprintf(stderr, "\e[0;31mFailed to create window\e[0m: %s", SDL_GetError());
         exit(1);
     }
 
@@ -22,14 +22,14 @@ void intitialise_screen(SDL_Renderer *renderer, SDL_Window *window, SDL_Texture 
 
     if(!renderer)
     {
-        fprintf(stderr, "\e[0;31mFailed to create renderer\e[0m", SDL_GetError());
+        fprintf(stderr, "\e[0;31mFailed to create renderer\e[0m: %s", SDL_GetError());
         exit(1);
     }
 
 	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     if(!texture) {
-        fprintf(stderr, "\e[0;31mFailed to create texture\e[0m", SDL_GetError());
+        fprintf(stderr, "\e[0;31mFailed to create texture\e[0m: %s", SDL_GetError());
         exit(1);
     }
 
