c4648285d45ed1ed5d509b6fb824e5177c19257e
commenthropha <aharoon1007@gmail.com>
2024-06-18 18:41:54 +0100

    Add window height/width macros to screen.h

--- src/screen.h
+++ src/screen.h
@@ -2,6 +2,9 @@
 #define SCREEN_H
 #include <SDL2/SDL.h>
 
+#define WINDOW_HEIGHT 320
+#define WINDOW_WIDTH 600
+
 int intitialise_screen(SDL_Renderer *renderer, SDL_Window *window, SDL_Texture *screen);
 
 #endif
