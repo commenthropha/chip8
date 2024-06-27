b0446304a2499b31e1ccc59f5f713e0eb4ab7921
Alexey-Savelyev-0 <alexeysavel@proton.me>
2024-06-27 09:52:15 +0100

    Added updated main.c code

--- src/main.c
+++ src/main.c
@@ -1,19 +1,48 @@
 #include "cpu.h"
 #include "screen.h"
 
+
 int main() {
     struct CPU cpu;
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
+    SDL_Event event;
+    int running = 1;
 
-    initialise_screen(&window, &renderer, &texture);
-    draw_test(window, renderer, texture);
+    // Initialize the CPU and screen
     initialise_cpu(&cpu);
+    initialise_screen(&window, &renderer, &texture);
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            running = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        draw_test(window, renderer, texture);
+    }
 
     free(cpu.memory);
     free(cpu.registers);
     free(cpu.stack);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
